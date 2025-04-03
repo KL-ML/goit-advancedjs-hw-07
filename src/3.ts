c2f0808e934f3e47fdda9b750c2ed8e44c305c6e
KL-ML <knataly0513@gmail.com>
2025-04-03 14:42:13 +0300

    Update 3.ts

--- src/3.ts
+++ src/3.ts
@@ -7,6 +7,17 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
+
+interface ICharacter {
+  name: string,
+  level: number,
+  introduce(phrase: string): void,
+  castSpell(): void
+}
+
+interface ISpellCaster {
+  levelUp(): void
+}
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
